341eed2fd3237cdc1c8e63563e39271ac7bb768f
Arifuddin Ahmad <arifuddinahmad@Arifuddins-MacBook-Pro.local>
2021-01-05 14:52:02 +0800

    Update health response

--- app/Http/Controllers/HealthController.php
+++ app/Http/Controllers/HealthController.php
@@ -7,7 +7,7 @@ class HealthController extends Controller{
 
     public function health(){
         return response()->json([
-            "health"=>"1.0.0.3"
+            "tag"=>"1.0.0.3"
         ]);
     }
 
